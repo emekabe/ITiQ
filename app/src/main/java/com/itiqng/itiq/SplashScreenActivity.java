94741f644df8589eb0a845fe6d3f9f782f91a63b
Emeka Chukwulobe <emekabe@yahoo.com>
2020-06-09 19:23:15 +0100

    Splash screen finish

--- app/src/main/java/com/itiqng/itiq/SplashScreenActivity.java
+++ app/src/main/java/com/itiqng/itiq/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.itiqng.itiq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -9,5 +10,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
